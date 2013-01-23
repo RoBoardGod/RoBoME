b4f65a3e97017a839a70e6c936a51ce8aab5f3d5
Jia Siang Tian <loveshawn0913@gmail.com>
2013-01-23 14:47:58 +0800

    RoBoME

---
+++ RoBoME_ver1.0/ME_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoBoME_ver1._0
+{
+    public class ME_Trigger
+    {
+        public string key;
+        public ME_Motion motion;
+        public ME_Trigger()
+        {
+            this.key = null;
+            this.motion = null;
+        }
+    }
+}
---
+++ RoBoME_ver1.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RoBoME_ver1._0
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            SetForm sform = new SetForm();
+            Form1 f1 = new Form1();
+            sform.ShowDialog();
+            if (sform.DialogResult == DialogResult.Yes)
+            {
+                f1.onPC = true;
+                Application.Run(f1);
+            }
+            else if (sform.DialogResult == DialogResult.No)
+            {
+
+                f1.onPC = false;
+                Application.Run(f1);
+            
+            }
+        }
+    }
+}
